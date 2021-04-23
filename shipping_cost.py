e9684ab591940bc860cec5b423840edce1109045
FModabber <76422648+FModabber@users.noreply.github.com>
2021-04-23 06:28:19 -0700

    update?

---
+++ shipping_cost.py
@@ -0,0 +1,8 @@
+'''build a program that will take the weight of a package and 
+determine the cheapest way to ship that package using Sal’s Shippers.
+more details -> codecademy/workflow projects
+'''
+weight = 10
+ #Ground Shipping
+if weight >= 10:
+    pass
