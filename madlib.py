810d251863a6c028add6c8bfd3ee2c8f0a55afd7
FModabber <76422648+FModabber@users.noreply.github.com>
2021-05-08 10:41:41 -0700

    concatination

---
+++ madlib.py
@@ -0,0 +1,13 @@
+#String concatenation
+# name = input("What is your name? ")
+# print ("Hi " + name)
+# print(f"Hi {name}")
+# print("Hi {}".format(name))
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay {verb2} like you are {famous_person}!"
+print(madlib)
